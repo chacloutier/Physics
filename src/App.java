7c96eaf3f4fdf1c4fcd49393798d787e5c38b562
DavidLeBon2001 <70335023+DavidLeBon2001@users.noreply.github.com>
2021-03-04 09:41:34 -0500

    message

--- src/App.java
+++ src/App.java
@@ -1,3 +1,9 @@
+//git add .
+//git commit m- "message"
+//git push
+
+//git pull
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
