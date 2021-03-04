b509b998087bd2ea8092e032eb673453cd283dfa
DavidLeBon2001 <70335023+DavidLeBon2001@users.noreply.github.com>
2021-03-04 13:07:42 -0500

    m

--- src/App.java
+++ src/App.java
@@ -1,11 +1,24 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 //git add .
 //git commit m- "message"
 //git push
 
 //git pull
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class App extends Application {
+    
+    public void start(Stage stage){
+        Button button= new Button("Hi");
+        Scene scene=new Scene(button, 400, 400);
+        stage.setTitle("Hello world");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[]args) throws Exception{
+        launch(args);
     }
 }
