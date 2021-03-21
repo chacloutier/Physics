aa395ae919e11f04161a48a359f0c760c519456f
DavidLeBon2001 <70335023+DavidLeBon2001@users.noreply.github.com>
2021-03-21 11:14:47 -0400

    oueee

--- src/App.java
+++ src/App.java
@@ -3,9 +3,14 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
@@ -22,20 +27,64 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    private Group group;
+    private Scene scene;
     public static void main(String[] args) {
         launch(args);
     }
     
     @Override
     public void start(Stage stage) {
-        stage.setTitle("Projectile motion");
 
-        
-  
+        Button pb = new Button("Parabolic motion");
+            pb.setPrefSize(250, 20);
+            
+            pb.setOnAction(value -> {
+            stage.setTitle("Projectile motion");
+
+            Display d = new Display();
+            stage.setScene(d.getScene());
+            stage.show();
+        });
+
+        Button sp = new Button("System of pulley");
+            sp.setPrefSize(250, 20);
+            sp.setOnAction(value -> {
+            stage.setTitle("System of pulley");
+
+            stage.show();
+        });
+
+
+
+        Button ip = new Button("Incline plane");
+            ip.setPrefSize(250, 20);
+            ip.setOnAction(value -> {
+            stage.setTitle("Incline Plane");
+
+            stage.show();
+        });
+
+        HBox layout= new HBox();
+        layout.setPrefHeight(50);
+        layout.setPrefWidth(50);
+        layout.setSpacing(10);
+        layout.setPadding(new Insets(0, 20, 10, 20)); 
         
 
-        Display d = new Display();
-        stage.setScene(d.getScene());
+        layout.getChildren().addAll(pb, ip, sp);
+        layout.setAlignment(Pos.CENTER);
+        
+        Scene scene1= new Scene(layout, 1000, 1000);
+        scene1.setFill(Color.BLACK);
+        
+
+        stage.setScene(scene1);
+        
+        stage.setHeight(1000);
+        stage.setWidth(1000);
         stage.show();
+        
     }
+    
 }
